2fbdd84e0df1e9d2ef4f10afd12c5e9fddbdf921
renato <renato.gamici@gmail.com>
2019-11-28 14:46:50 -0300

    Modified strings

--- README.md
+++ README.md
@@ -1,25 +1,25 @@
-# anki_plugin_jaja_definitions
+# anki\_plugin\_jaja\_definitions
 
-An Anki add-on, based on the <a href="https://ankiweb.net/shared/info/1967553085" rel="nofollow">Sanseido Definitions</a> add-on, for adding Japanese translations of Korean vocabulary. It's primarily meant for Korean learners who're proficient in or simultaneously learning, Japanese.
+An Anki add-on, based on the [Japanese Definitions for Korean Vocabulary](https://github.com/steviepoppe/anki_plugin_jk_definitions) add-on (which is based on [Sanseido Definitions](https://ankiweb.net/shared/info/1967553085) add-on), for adding Japanese definitions of Japanese vocabulary. 
+It's primarily meant for learners who already had some grasp of Japanese.
 
-The add-on crawls Weblio's Korean-Japanese dictionary and scrapes each translation. As 개 could mean either dog, or be used as a particular counter, the field containing the Japanese definitions will look like this:
+The add-on crawls Weblio's dictionary and scrapes each definition. 
 
-* (1) 犬
-* (2) 個 
+\[Add example]
 
 ## Requirements
 
 The add-on requires two fields:
 
-<ul>
-<li>**Korean**: the field containing your Korean expression.</li>
-<li>**Japanese**: the field that'll contain the Japanese translations.</li>
-</ul>
+* **Japanese**: the field containing your Japanese expression.
+* **JapaneseDefinition**: the field that'll contain the Japanese definitions.
 
-These names can be changed to your liking by opening the *JapaneseDefinitionsKoreanVoc.py* file with a text-editor and editting `expressionField = 'Korean'` and `jap_transField = 'Japanese'. The file can be found in your anki add-ons folder.
+These names can be changed to your liking by opening the *__init__.py* file with a text-editor and editting `expressionField = Japanese'` and `jap_defField = 'JapaneseDefinition'`.
+The file can be found in your anki add-ons folder.
 
 ## Usage
 
-Just select the cards you'd like to adjust in Anki's Browser and select **Edit** -&gt; **Regenerate JK References**. Do note each definition is pulled from Weblio's online dictionary, so if you're bulk-editting thousands of cards, this could take a while.
+Just select the cards you'd like to adjust in Anki's Browser and select **Edit** -&gt; **Regenerate Ja-Ja References**. 
+Do note each definition is pulled from Weblio's online dictionary, so if you're bulk-editting thousands of cards, this could take a while.
 
-For questions or support, please refer to this plugin's <a href="https://github.com/steviepoppe/anki_plugin_jk_definitions" rel="nofollow">github page</a>.
+For questions or support, please refer to this plugin's [github page](https://github.com/steviepoppe/anki_plugin_jk_definitions).
--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 # -*- mode: Python ; coding: utf-8 -*-
 #
-# https://github.com/steviepoppe/anki_plugin_remove_missing_audio_references
-# Authors: Stevie Poppe
+# https://github.com/rgamici/anki_plugin_jaja_definitions
+# Authors: renato
 #
-# Description: Plugin to add Japanese translations on Korean vocabulary,
-# based on the Sanseido Definitions plugin for Anki
-# pulls definitions from Weblio's KJ-JK dictionary.
+# Description: Plugin to add Japanese definition on Japanese vocabulary,
+# based on the Japanese Definitions for Korean Vocabulary plugin for Anki
+# pulls definitions from Weblio's dictionary.
 
 from bs4.BeautifulSoup import BeautifulSoup
 import urllib.request
@@ -22,8 +22,8 @@ from anki.notes import Note
 from aqt import mw
 
 # Edit these field names if necessary =========================================
-expressionField = 'Korean'
-jap_transField = 'Japanese'
+expressionField = 'Japanese'
+jap_defField = 'JapaneseDefinition'
 # ==============================================================================
 
 # Fetch definition from Weblio ================================================
@@ -31,19 +31,19 @@ jap_transField = 'Japanese'
 
 def fetchDef(term):
     defText = ""
-    pageUrl = ("http://kjjk.weblio.jp/content/"
+    pageUrl = ("http://kjjk.weblio.jp/content/"  # change URL
                + urllib.parse.quote(term.encode('utf-8')))
     response = urllib.request.urlopen(pageUrl)
     soup = BeautifulSoup(response)
-    NetDicBody = soup.find('div', class_="kiji")
+    NetDicBody = soup.find('div', class_="kiji")  # OK
 
     if NetDicBody is not None:
-        test = NetDicBody.find_all('span', {"lang": "ja"})
+        test = NetDicBody.find_all('span', {"lang": "ja"})  # no such element
         counter = 1
 
         if test is not None:
             for line in test:
-                if str(line).find(term) == -1:
+                if str(line).find(term) == -1:  # require better formating
                     defText += "(" + str(counter) + ") " + str(line) + "<br/>"
                     counter = counter + 1
 
@@ -55,9 +55,9 @@ def fetchDef(term):
 
 
 def glossNote(f):
-    if f[jap_transField]:
+    if f[jap_defField]:
         return
-    f[jap_transField] = fetchDef(f[expressionField])
+    f[jap_defField] = fetchDef(f[expressionField])
 
 
 def setupMenu(ed):
@@ -67,7 +67,7 @@ def setupMenu(ed):
 
 
 def onRegenGlosses(ed):
-    n = "Regenerate KJ definitions"
+    n = "Regenerate Ja-Ja definitions"
     ed.editor.saveNow()
     regenGlosses(ed, ed.selectedNotes())
     mw.requireReset()
@@ -76,7 +76,7 @@ def onRegenGlosses(ed):
 def regenGlosses(ed, fids):
     mw.progress.start(max=len(fids), immediate=True)
     for (i, fid) in enumerate(fids):
-        mw.progress.update(label='Generating KJ definitions...', value=i)
+        mw.progress.update(label='Generating Ja-Ja definitions...', value=i)
         f = mw.col.getNote(id=fid)
         try:
             glossNote(f)
