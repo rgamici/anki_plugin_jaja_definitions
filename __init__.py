920770954ef5a2cd01e103d59112e6de85e7a7dd
renato <renato.gamici@gmail.com>
2019-11-28 13:10:57 -0300

    fixing some pep8 problems

--- __init__.py
+++ __init__.py
@@ -8,75 +8,88 @@
 # pulls definitions from Weblio's KJ-JK dictionary.
 
 from bs4.BeautifulSoup import BeautifulSoup
-from PyQt5.QtWidgets import *
-import urllib.request, urllib.parse, urllib.error
+import urllib.request
+import urllib.parse
+import urllib.error
 import re
 from aqt.utils import showInfo
 import string
-
-# Edit these field names if necessary ==========================================
-expressionField = 'Korean'
-jap_transField = 'Japanese'
-# ==============================================================================
-
-# Fetch definition from Weblio =================================================
-def fetchDef(term):
-	defText = ""
-	pageUrl = "http://kjjk.weblio.jp/content/" + urllib.parse.quote(term.encode('utf-8'))
-	response = urllib.request.urlopen(pageUrl)
-	soup = BeautifulSoup(response)
-	NetDicBody = soup.find('div', class_ = "kiji")
-
-	if NetDicBody is not None:
-		test = NetDicBody.find_all('span', {"lang":"ja"})
-		counter = 1
-
-		if test is not None:
-			for line in test:
-				if str(line).find(term) == -1:
-					defText += "(" + str(counter) + ") " + str(line) + "<br/>"
-					counter = counter + 1
-
-	if defText != "":
-		defText = string.replace(defText, ',', ', ')
-	return defText
-
-# Update note ==================================================================
+from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from anki.hooks import addHook
 from anki.notes import Note
 from aqt import mw
 
-def glossNote( f ):
-   if f[ jap_transField ]: return
-   f[ jap_transField ] = fetchDef( f[ expressionField ] )
+# Edit these field names if necessary =========================================
+expressionField = 'Korean'
+jap_transField = 'Japanese'
+# ==============================================================================
 
-def setupMenu( ed ):
-	a = QAction( 'Regenerate KJ definitions', ed )
-	a.triggered.connect(lambda e=ed: onRegenGlosses( e ))
-	ed.form.menuEdit.addAction( a )
+# Fetch definition from Weblio ================================================
 
-def onRegenGlosses( ed ):
-	n = "Regenerate KJ definitions"
-	ed.editor.saveNow()
-	regenGlosses(ed, ed.selectedNotes() )   
-	mw.requireReset()
-	
-def regenGlosses( ed, fids ):
-	mw.progress.start( max=len( fids ) , immediate=True)
-	for (i,fid) in enumerate( fids ):
-		mw.progress.update( label='Generating KJ definitions...', value=i )
-		f = mw.col.getNote(id=fid)
-		try: glossNote( f )
-		except:
-			import traceback
-			print('definitions failed:')
-			traceback.print_exc()
-		try: f.flush()
-		except:
-			raise Exception()
-		ed.onRowChanged(f,f)
-	mw.progress.finish()
-	
-addHook( 'browser.setupMenus', setupMenu )
+
+def fetchDef(term):
+    defText = ""
+    pageUrl = ("http://kjjk.weblio.jp/content/"
+               + urllib.parse.quote(term.encode('utf-8')))
+    response = urllib.request.urlopen(pageUrl)
+    soup = BeautifulSoup(response)
+    NetDicBody = soup.find('div', class_="kiji")
+
+    if NetDicBody is not None:
+        test = NetDicBody.find_all('span', {"lang": "ja"})
+        counter = 1
+
+        if test is not None:
+            for line in test:
+                if str(line).find(term) == -1:
+                    defText += "(" + str(counter) + ") " + str(line) + "<br/>"
+                    counter = counter + 1
+
+    if defText != "":
+        defText = string.replace(defText, ',', ', ')
+    return defText
+
+# Update note =================================================================
+
+
+def glossNote(f):
+    if f[jap_transField]:
+        return
+    f[jap_transField] = fetchDef(f[expressionField])
+
+
+def setupMenu(ed):
+    a = QAction('Regenerate KJ definitions', ed)
+    a.triggered.connect(lambda e=ed: onRegenGlosses(e))
+    ed.form.menuEdit.addAction(a)
+
+
+def onRegenGlosses(ed):
+    n = "Regenerate KJ definitions"
+    ed.editor.saveNow()
+    regenGlosses(ed, ed.selectedNotes())
+    mw.requireReset()
+
+
+def regenGlosses(ed, fids):
+    mw.progress.start(max=len(fids), immediate=True)
+    for (i, fid) in enumerate(fids):
+        mw.progress.update(label='Generating KJ definitions...', value=i)
+        f = mw.col.getNote(id=fid)
+        try:
+            glossNote(f)
+        except:
+            import traceback
+            print('definitions failed:')
+            traceback.print_exc()
+        try:
+            f.flush()
+        except:
+            raise Exception()
+        ed.onRowChanged(f, f)
+    mw.progress.finish()
+
+
+addHook('browser.setupMenus', setupMenu)
